dcbe4132cb30c4c62f2fcccd3be1349c50c32194
Monica Stigliano <monicastigliano@gmail.com>
2023-07-27 10:59:30 -0300

    Proyecto Integrador - Etapa 4

---
+++ Desarrollo-Web-con-HTML/Proyecto Integrador/interfaz web/php/codigo.php
@@ -0,0 +1,24 @@
+<?php
+$to = "monicastigliano@gmail.com";
+$subject = "Mail desde el formulario";
+$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-type:text/html;charset=UTF-8" .
+"\r\n";
+$nombre = $_POST['nombre'];
+$email = $_POST['email'];
+$comentarios = $_POST['comentarios'];
+$message = "
+<html>
+<head>
+<title>HTML</title>
+</head>
+<body>
+<h1>Información del formulario</h1>
+<p>Nombre: $nombre </p>
+<p>Email: $email </p>
+<p>Comentarios: $comentarios </p>
+</body>
+</html>";
+ echo 'Gracias por comunicarse con nosotros';
+mail($to, $subject, $message, $headers);
+?>
